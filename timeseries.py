d9b52d740b983ce449668552db0ccae3c40ebcf9
Vadym Barda <vadym@kensho.com>
2016-05-02 23:35:03 -0400

    added metric combine function

--- timeseries.py
+++ timeseries.py
@@ -18,3 +18,21 @@ def get_metrics(symbol, start_date=None, end_date=None, ts_getter=get_historical
     ts_df = pd.DataFrame.from_dict(ts_dict, orient='index').astype(float)
     ts_df.index = pd.to_datetime(ts_df.index)
     return ts_df
+
+
+def combine_metrics(symbols, col_name='Adj Close', **kwargs):
+    # TODO: add test
+    '''Creates a combined df for an iterable of tickers by picking
+    specific column from get_metrics output
+    Args:
+        symbols: (str/iterable) of tickers to get the metric for
+        col_name: (str) name of column from the get_metrics output to be used.
+            Defaults to 'Adj Close'
+        **kwargs: start_date, end_date and ts_getter(ystockquote function) for get_metrics
+    '''
+    if isinstance(symbols, str) or isinstance(symbols, unicode):
+        symbols = [symbols]
+    else:
+        symbols = list(symbols)
+    return pd.DataFrame.from_dict({symbol: get_metrics(symbol, **kwargs)[col_name]
+                                   for symbol in symbols})
