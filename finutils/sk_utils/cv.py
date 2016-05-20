df6066a1dc5a262c68a15030a4240dbadc80d585
Vadym Barda <vadim.barda@gmail.com>
2016-05-20 09:56:53 -0400

    added cross_validation to sk_utils and wrote a test

---
+++ finutils/sk_utils/cv.py
@@ -0,0 +1,25 @@
+import numpy as np
+from sklearn.cross_validation import train_test_split
+
+DEFAULT_TRAIN = .7
+
+
+class CV(object):
+    def __init__(self, df, X_cols, y_col, train_size=DEFAULT_TRAIN):
+        '''Split the dataframe with features (X_cols) and target (y_col) into
+        train and test samples
+        Args:
+            df: (pd.DataFrame) with features and target
+            X_cols: (iterable) of feature columns names
+            y_col: (str) target column name
+            train_size: (float) the size of the train sample.
+                passed to sklearn.cross_validation.train_test_split
+        '''
+        self.df = df
+        self.X_cols = X_cols
+        self.X = np.array(df[X_cols])
+        self.y = np.array(df[y_col])
+        (self.X_tr,
+         self.X_te,
+         self.y_tr,
+         self.y_te) = train_test_split(self.X, self.y, train_size=train_size)
--- tests/test_all.py
+++ tests/test_all.py
@@ -1,9 +1,11 @@
 import datetime
 import pandas as pd
+import numpy as np
 import unittest
 
 from finutils.utils import column_renamer, lower_func, space_replace_func
 from finutils.ingest.timeseries import get_metrics
+from finutils.sk_utils.cv import CV
 
 
 class TestUtils(unittest.TestCase):
@@ -33,3 +35,15 @@ class TestMetrics(unittest.TestCase):
 
         desired_columns = ['Volume', 'Adj Close', 'High', 'Low', 'Close', 'Open']
         self.assertListEqual(sorted(desired_columns), sorted(ts.columns))
+
+
+class TestCV(unittest.TestCase):
+
+    def test_split(self):
+        df = get_metrics('AAPL').pipe(column_renamer)
+        X_cols = df.columns.drop('close')
+        cv = CV(df, X_cols=X_cols, y_col='close', train_size=.8)
+        self.assertTupleEqual(df.drop('close', 1).shape, cv.X.shape)
+        self.assertTupleEqual(
+            np.concatenate([cv.X_tr, cv.X_te]).shape, cv.X.shape
+        )
